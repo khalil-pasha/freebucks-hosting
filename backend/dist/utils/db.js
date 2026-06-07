c6fb4679dd3cbdcecbf8f9603eb7fa5489323c07
khalil-pasha <krehmanvlogs@gmail.com>
2026-06-07 14:27:07 +0530

    Fix Prisma v7 adapter initialization

--- backend/dist/utils/db.js
+++ backend/dist/utils/db.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 const client_1 = require("@prisma/client");
-exports.db = global.prisma || new client_1.PrismaClient();
+const pg_1 = require("pg");
+const adapter_pg_1 = require("@prisma/adapter-pg");
+const connectionString = process.env.DATABASE_URL;
+const pool = new pg_1.Pool({ connectionString });
+const adapter = new adapter_pg_1.PrismaPg(pool);
+exports.db = global.prisma || new client_1.PrismaClient({ adapter });
 if (process.env.NODE_ENV !== 'production') {
     global.prisma = exports.db;
 }
--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 // Prevent multiple instances of Prisma Client in development
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = global.prisma || new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const db = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = db;
